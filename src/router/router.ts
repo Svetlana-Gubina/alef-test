5d29ff159c955d6f3be2997657510a06fe1a0d00
Svetlana-Gubina <gubinas83@gmail.com>
2024-11-07 23:05:20 +0300

    fix deploy

--- src/router/router.ts
+++ src/router/router.ts
@@ -1,24 +1,24 @@
-import { createWebHistory, createRouter } from "vue-router";
+import { createWebHistory, createRouter } from 'vue-router';
 
 const router = createRouter({
   routes: [
     {
-      path: "/",
-      name: "Home",
-      component: () => import("../components/HomeVue.vue"),
+      path: '/',
+      name: 'Home',
+      component: () => import('../components/HomeVue.vue'),
     },
     {
-      path: "/preview",
-      name: "Preview",
-      component: () => import("../components/Preview.vue"),
+      path: '/preview',
+      name: 'Preview',
+      component: () => import('../components/Preview.vue'),
     },
     {
-      path: "/:pathMatch(.*)*",
-      name: "Not found",
-      component: () => import("../components/PageNotFound.vue"),
+      path: '/:pathMatch(.*)*',
+      name: 'Not found',
+      component: () => import('../components/PageNotFound.vue'),
     },
   ],
-  history: createWebHistory("/"),
+  history: createWebHistory(import.meta.env.BASE_URL),
 });
 
 export default router;
